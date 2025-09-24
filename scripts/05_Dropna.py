73858cdd69c25c2cbb7acc36afdd1176bd48a5e4
ph-tech1 <plinhares832@gmail.com>
2025-09-24 00:12:34 -0300

    Atualizando arquivo 05

--- scripts/05_Dropna.py
+++ scripts/05_Dropna.py
@@ -4,15 +4,20 @@ import pandas as pd
 caminho_arquivo = './data/games.xlsx'
 df = pd.read_excel(caminho_arquivo)
 
-# Filtrando quais plataformas por soma total das vendas e a media
-vendas_por_plataforma = df.groupby('Platform')['Global_Sales'].agg(['sum', 'mean'])
+# --- Limpeza dos Dados ---
+# 1. Criamos um DataFrame limpo removendo as linhas onde o ano é nulo
+df_limpo = df.dropna(subset=['Year'])
 
-# Ordenando em ordem decrescente pela coluna 'sum'
-vendas_por_plataforma_ordenado = vendas_por_plataforma.sort_values(by='sum', ascending=False)
+# 2. Na cópia limpa, convertemos a coluna 'Year' para inteiro
+df_limpo['Year'] = df_limpo['Year'].astype(int)
 
-# Mostrando o resultado
-print("---------- Vendas Totais e Médias por Plataforma ----------")
-print(vendas_por_plataforma_ordenado)
+# --- Análise com os Dados Limpos ---
+# Agora usamos 'df_limpo' para a análise
+vendas_por_ano = df_limpo.groupby('Year')['Global_Sales'].sum()
+vendas_por_ano_ordenado = vendas_por_ano.sort_values(ascending=False)
 
-# Remove todas as linhas que têm um valor NaN na coluna 'Year'
-dataframe_limpo = df.dropna(subset=['Year'])
+print("---------- Ranking de Vendas Globais por Ano ----------")
+print(vendas_por_ano_ordenado)
+
+# Dica Bônus: Para ver apenas o ano campeão
+print("\nO ano de ouro dos games foi:", vendas_por_ano_ordenado.index[0])
