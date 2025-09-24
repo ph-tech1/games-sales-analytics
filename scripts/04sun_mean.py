e7d92a8b79df34b418a8492bb3c8f2d1de91c60e
ph-tech1 <plinhares832@gmail.com>
2025-09-23 23:42:13 -0300

    criação do arquivo sun_mean

---
+++ scripts/04sun_mean.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Chamando a tabela
+caminho_arquivo = './data/games.xlsx'
+df = pd.read_excel(caminho_arquivo)
+
+# Filtrando quais plataformas por soma total das vendas e a media
+vendas_por_plataforma = df.groupby('Platform')['Global_Sales'].agg(['sum', 'mean'])
+
+# Ordenando em ordem decrescente pela coluna 'sum'
+vendas_por_plataforma_ordenado = vendas_por_plataforma.sort_values(by='sum', ascending=False)
+
+# Mostrando o resultado
+print("---------- Vendas Totais e Médias por Plataforma ----------")
+print(vendas_por_plataforma_ordenado)
